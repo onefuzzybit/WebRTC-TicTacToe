df2188b07b8563584460671d662302eb220f386e
One Fuzzy Bit <onefuzzybit@gmail.com>
2023-04-01 15:24:58 +0000

    improved dev workflow - auto build dependants on every change

--- tic-tac-toe-ms1/vite.config.ts
+++ tic-tac-toe-ms1/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+	alias: {
+		'signalling-connect': resolve(__dirname, '../signalling-connect/src/connect.ts'),
+		'signalling-client': resolve(__dirname, '../signalling-client/src/client.ts'),
+	},
+  },
   plugins: [react()],
 })
